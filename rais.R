23b6f1b5282083171d5b4bc2a5a0f6c3abb0adae
mandicneves <mandicneves@gmail.com>
2025-03-15 21:41:36 -0300

    intial analysis on data

--- rais.R
+++ rais.R
@@ -1,6 +1,6 @@
 # loading packages ####
 library(basedosdados)
-library(dplyr)
+library(tidyverse)
 
 
 # setting inital setup ####
@@ -9,8 +9,42 @@ file.path(getwd(), ".keys") |>
   trimws() |> 
   basedosdados::set_billing_id()
 
-# analysis of labor market evolution by sector
-dados_evolucao_mt <- basedosdados::bdplyr("basedosdados.br_me_rais.microdados_vinculos")
+# analysis of labor market evolution by sector in sao paulo city ####
 
-dados_evolucao_mt |> glimpse()
+dicionario_rais <- basedosdados::bdplyr("basedosdados.br_me_rais.dicionario") |>
+  basedosdados::bd_collect()
+dicionario_rais |> glimpse()
 
+dados_rais <- basedosdados::bdplyr("basedosdados.br_me_rais.microdados_vinculos")
+dados_rais |> 
+  glimpse()
+dados_evolucao_mt <- dados_rais |> 
+  filter(
+    ano >= 2020 & ano <= 2023,
+    id_municipio == "3550308"
+  ) |> 
+  select(
+    ano,
+    cbo_2002,
+    cnae_1,
+    vinculo_ativo_3112,
+    valor_remuneracao_media_sm
+   ) |>
+  basedosdados::bd_collect()
+  
+
+tabela_total_empregos <- dados_evolucao_mt |>
+  group_by(ano, cnae_1) |>
+  summarise(total_empregos = sum(vinculo_ativo_3112 |> as.numeric())) |>
+  arrange(ano, desc(total_empregos))
+
+
+tabela_total_empregos |>
+  # filter(total_empregos >= mean(tabela_total_empregos$total_empregos)) |>
+  ggplot(aes(x = ano, y = total_empregos, color = cnae_1)) +
+  geom_area() +
+  labs(title = "Evolução do Emprego Formal por Setor",
+       x = "Ano",
+       y = "Número de Empregos") +
+  theme_minimal() +
+  theme(legend.position = "none")
